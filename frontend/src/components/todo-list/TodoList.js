a080aa0a5e69746a536dc1ddbb6ade30df5f2378
chandra8997 <87073900+chandra8997@users.noreply.github.com>
2022-06-26 22:39:36 +0530

    Added files

---
+++ frontend/src/components/todo-list/TodoList.js
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import {
+  clearCurrentTodo,
+  createTodo,
+  getTodos,
+  updateTodo,
+} from "../../redux/actions/todos";
+import Input from "../common/form/Input";
+import Paper from "../common/ui/Paper";
+import Todo from "./todo/Todo";
+const TodoList = () => {
+  const { todos, currentTodo, loadMore } = useSelector((state) => state.todos);
+
+  const [initialValues, setInitialValues] = useState({
+    title: "",
+    description: "",
+  });
+
+  const [todoLoading, setTodoLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTodos(page));
+  }, [page]);
+
+  const loadMoreTodos = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: Yup.object({
+      title: Yup.string().required("Required"),
+      description: Yup.string().required("Required"),
+    }),
+    enableReinitialize: true,
+    onSubmit: async (values) => {
+      setTodoLoading(true);
+      if (currentTodo) {
+        const payload = {
+          _id: currentTodo._id,
+          title: values.title,
+          description: values.description,
+        };
+        await dispatch(updateTodo(payload));
+        await dispatch(clearCurrentTodo());
+      } else {
+        await dispatch(createTodo(values));
+      }
+      setTodoLoading(false);
+      formik.values.title = "";
+      formik.values.description = "";
+    },
+  });
+
+  return (
+    <div className="flex items-start justify-center h-full p-4 mb-4">
+      <Paper className="w-full overflow-hidden rounded-xl sm:w-1/2 lg:w-1/3 xl:w-1/3">
+        <form
+          className="flex flex-col items-end gap-6 p-6 bg-teal-500/30"
+          onSubmit={formik.handleSubmit}>
+          <Input
+            placeholder="Enter title"
+            id="title"
+            name="title"
+            type="text"
+            width="full"
+            onChange={formik.handleChange}
+            // onBlur={formik.handleBlur}
+            value={formik.values.title}
+            formik={formik}
+          />
+          <Input
+            placeholder="Enter description"
+            id="description"
+            name="description"
+            type="text"
+            width="full"
+            required
+            onChange={formik.handleChange}
+            // onBlur={formik.handleBlur}
+            value={formik.values.description}
+            formik={formik}
+          />
+          <button
+            type="submit"
+            disabled={todoLoading}
+            className="flex items-center justify-between gap-1 px-4 py-2 text-sm font-bold uppercase bg-teal-800 rounded-lg text-slate-50 dark:text-slate-200 dark:bg-slate-800 hover:bg-slate-700 disabled:bg-slate-600">
+            {!currentTodo ? "add" : "update"}
+            {todoLoading && (
+              <div className="w-4 h-4 rounded-full bg-gradient-to-r from-teal-200 to-teal-800 animate-spin"></div>
+            )}
+          </button>
+        </form>
+        <div className="flex flex-col divide-y-1 divide-slate-300 dark:divide-slate-700">
+          {todos.map((todo) => (
+            <Todo key={todo._id} todo={todo} changeValues={setInitialValues} />
+          ))}
+          {loadMore && (
+            <button
+              onClick={loadMoreTodos}
+              className="flex items-center justify-center px-4 py-2 text-xs font-bold uppercase bg-teal-800 rounded-lg rounded-t-none text-slate-50 dark:text-slate-200 dark:bg-slate-800 hover:bg-slate-700 disabled:bg-slate-600">
+              load more...
+            </button>
+          )}
+        </div>
+      </Paper>
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ frontend/src/components/todo-list/todo/Todo.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  clearCurrentTodo,
+  deleteTodo,
+  setCurrentTodo,
+  updateTodo,
+} from "../../../redux/actions/todos";
+const Todo = ({ todo, changeValues }) => {
+  const dispatch = useDispatch();
+
+  const [todoLoading, setTodoLoading] = useState("");
+
+  const handleDelete = async () => {
+    setTodoLoading("DELETE");
+    await dispatch(clearCurrentTodo());
+    await dispatch(deleteTodo(todo._id));
+    setTodoLoading("");
+  };
+
+  const handleEdit = async () => {
+    await changeValues({ title: todo.title, description: todo.description });
+    await dispatch(setCurrentTodo(todo));
+  };
+  return (
+    <div className="flex flex-row items-center justify-between gap-4 p-6 ">
+      <div className="flex flex-col start">
+        <div className="font-semibold dark:text-gray-300 text-md">
+          {todo.title}
+        </div>
+        <div className="text-sm dark:text-gray-400">{todo.description}</div>
+      </div>
+      <div className="flex flex-col items-end text-sm font-semibold">
+        <button
+          className="tracking-widest text-red-700 uppercase hover:text-red-500 dark:text-red-400 dark:hover:text-red-300"
+          onClick={handleDelete}>
+          {todoLoading === "DELETE" ? "deleting..." : "delete"}
+        </button>
+        <button
+          className="tracking-widest text-teal-800 uppercase hover:text-teal-500 dark:text-teal-400 dark:hover:text-teal-300"
+          onClick={handleEdit}>
+          edit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
